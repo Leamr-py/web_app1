cbd5ef25105ca3f08afbf0285bbd4a5be9bbb172
Austin Broders <austin@Austins-MacBook-Pro.local>
2020-03-07 20:13:48 +0900

    endpoints easier to read

--- views.py
+++ views.py
@@ -1,66 +1,54 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 import json
 import os
-
+from . import crud
+import sys
 
 main = Blueprint('main', __name__)
 
+#cannot define function and use in a route. try creating a module of functions and importing
 
 @main.route('/')
 def index():
-	#with open('list.json') as file:
-	#	items = json.load(file)
-	with open('lists.json') as file:
-		try:
-			lists = json.load(file)
-		except:
-			lists = ''
+	lists = crud.read_lists()
 	return render_template('homepage.html', lists=lists)
+
 
 @main.route('/list', methods=['POST'])
 def list():
 	name = request.form.get('chooselist')
-	with open('/Users/austin/src/web_app/lists/' + name + '.json') as file:
-		items = json.load(file)
+	items = crud.open_file(name)
 	return render_template('list.html', items=items, name=name)
 
-@main.route('/list_post', methods=['POST'])
-def list_post():
-	name = request.form.get('names')
-	with open('/Users/austin/src/web_app/lists/' + name + '.json') as file:
-		items = json.load(file)
+@main.route('/list_post/<name>', methods=['POST'])
+def list_post(name):
+
+	items = crud.open_file(name)
 	list_item = request.form.get('items')
 	if list_item:
 		items.append(f'{list_item}')
-	with open('/Users/austin/src/web_app/lists/' + name + '.json', 'w') as file:
-		json.dump(items, file)
+	crud.append_file(name, items)
 	return render_template('list.html',items=items, name=name)
 
-@main.route('/drop_down_remove', methods=['POST'])
-def drop_down():
-	name = request.form.get('names')
-	with open('/Users/austin/src/web_app/lists/' + name + '.json') as file:
-		items = json.load(file)
+@main.route('/drop_down_remove/<name>', methods=['POST'])
+def drop_down(name):
+	
+	items = crud.open_file(name)
 	list_remove = request.form.get('drop_down')
 	if list_remove:
 		items.remove(f'{list_remove}')
-	with open('/Users/austin/src/web_app/lists/' + name + '.json', 'w') as file:
-		json.dump(items, file)
+	crud.append_file(name, items)
 	return render_template('list.html', items=items, name=name)
 
 @main.route('/create_list', methods=['POST'])
 def create_list():
 	name = request.form.get('new_list')
-	with open('/Users/austin/src/web_app/lists/' + name + '.json', 'w') as file:
-		json.dump([], file)
+	crud.append_file(name, [])
 	
-	with open('lists.json') as file:
-		lists = json.load(file)
-		lists.append(f'{name}')
-	with open('lists.json', 'w') as file:
-		json.dump(lists, file)
-	with open('/Users/austin/src/web_app/lists/' + name + '.json') as file:
-		items = json.load(file)
+	lists = crud.read_lists()
+	lists.append(f'{name}')
+	crud.append_lists(lists)
+	items = crud.open_file(name)
 	return render_template('list.html', items=items, name=name)
 
 @main.route('/delete_list', methods=['POST'])
@@ -68,19 +56,14 @@ def delete_list():
 	name = request.form.get('current_lists')
 	try:
 		os.remove('/Users/austin/src/web_app/lists/' + name + '.json')
-		with open('lists.json') as file:
-			lists = json.load(file)
-			lists.remove(f'{name}')
-		with open('lists.json', 'w') as file:
-			json.dump(lists, file)
+		lists = crud.read_lists()
+		lists.remove(f'{name}')
+		crud.append_lists(lists)
 		return render_template('homepage.html', lists=lists)
 	except FileNotFoundError:
-		with open('lists.json') as file:
-			lists = json.load(file)
+		lists =  crud.read_lists()
 		return render_template('homepage.html', lists=lists)
 	except TypeError:
-		with open('lists.json') as file:
-			lists = json.load(file)
+		lists = crud.read_lists()
 		return render_template('homepage.html', lists=lists)
-	
 
